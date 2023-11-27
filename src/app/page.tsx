12a8c29a997b681cfb635fe757b36f945f0ca159
makkenzo <nekgo2009@gmail.com>
2023-11-27 15:25:48 +0600

    auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
+'use client';
+
 import { Header, MainContainer } from '@/components';
 import React from 'react';
+import { AnimatePresence } from 'framer-motion';
 
 const HomePage = () => {
     return (
-        <div className="w-screen h-auto flex flex-col bg-primary">
-            <Header />
+        <AnimatePresence>
+            <div className="w-screen h-auto flex flex-col bg-primary">
+                <Header />
 
-            <main className="mt-24 p-8 w-full">
-                <MainContainer />
-            </main>
-        </div>
+                <main className="mt-24 p-8 w-full">
+                    <MainContainer />
+                </main>
+            </div>
+        </AnimatePresence>
     );
 };
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,29 @@
+'use client';
+
 import Image from 'next/image';
 import { MdShoppingBasket } from 'react-icons/md';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { app } from '../../firebase.config';
 
 const Header = () => {
+    const firebaseAuth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    const login = async () => {
+        const response = await signInWithPopup(firebaseAuth, provider);
+        console.log(response);
+    };
+
     return (
         <header className="w-screen fixed z-50 p-6 px-16">
             {/* desktop & tablet */}
             <div className="hidden md:flex w-full h-full items-center justify-between">
-                <div className="flex items-center gap-2">
+                <Link href="/" className="flex items-center gap-2">
                     <Image src="/img/logo.png" alt="logo" width={32} height={32} className="object-cover" />
                     <p className="text-headingColor text-xl font-bold"> FoodZero</p>
-                </div>
+                </Link>
                 <div className="flex items-center gap-8">
                     <ul className="flex items-center gap-8">
                         <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
@@ -33,13 +47,15 @@ const Header = () => {
                         </div>
                     </div>
 
-                    <Image
-                        src="/img/avatar.png"
-                        alt="user_profile"
-                        width={40}
-                        height={40}
-                        className="min-w-[40px] min-h-[40px] drop-shadow-xl"
-                    />
+                    <div className="relative">
+                        <motion.img
+                            whileTap={{ scale: 0.6 }}
+                            src="/img/avatar.png"
+                            alt="user_profile"
+                            className="min-w-[40px] w-10 min-h-[40px] h-10 drop-shadow-xl cursor-pointer"
+                            onClick={login}
+                        />
+                    </div>
                 </div>
             </div>
 

3ae0cef04d751c8266df2685e1f2e4c2c8d45109
makkenzo <nekgo2009@gmail.com>
2023-12-14 19:02:13 +0600

    my orders styled

--- src/components/ordersContainer.tsx
+++ src/components/ordersContainer.tsx
@@ -28,73 +28,95 @@ const OrdersContainer = () => {
         }
     }, [uid]);
 
+    const formatDateTime = (date: Date) => {
+        const options: Intl.DateTimeFormatOptions = {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false,
+        };
+
+        return new Intl.DateTimeFormat('en-US', options).format(date);
+    };
+
     return (
         <div className="container mx-auto p-4">
             <h2 className="text-3xl font-bold mb-8 text-center">Мои заказы</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {userOrders &&
-                    userOrders.map((order) => (
-                        <div
-                            key={order.orderId}
-                            className="bg-white rounded-lg overflow-hidden shadow-md p-6 flex flex-col justify-between"
-                        >
-                            <div>
-                                <p className="text-lg font-semibold">Заказ №: {order.orderId}</p>
+                {userOrders && userOrders.length > 0 ? (
+                    userOrders
+                        .slice()
+                        .reverse()
+                        .map((order) => (
+                            <div
+                                key={order.orderId}
+                                className="bg-white rounded-lg overflow-hidden shadow-md p-6 flex flex-col justify-between"
+                            >
+                                <div>
+                                    <p className="text-lg font-semibold">Заказ №: {order.orderId}</p>
 
-                                <p className="text-gray-600 flex">
-                                    <FaSackDollar className="mr-2 mt-0.5" /> Итоговая цена: {order.totalPrice} ₸
-                                </p>
-                                <p className="text-gray-600 flex">
-                                    <FaBowlFood className="mr-2 mt-0.5" /> Статус заказа:{' '}
-                                    {order.orderStatus === 'pending' ? 'В обработке' : order.orderStatus}
-                                </p>
-                                <p className="text-gray-600 flex">
-                                    <TbTruckDelivery className="mr-2 mt-0.5" />
-                                    Статус доставки:{' '}
-                                    {order.deliveryStatus === 'pending' ? 'В обработке' : order.orderStatus}
-                                </p>
-                                <p className="text-gray-600 flex">
-                                    <FaClipboardList className="mr-2 mt-0.5" />
-                                    Инструкции курьеру:{' '}
-                                    {order.deliveryInstructions !== ''
-                                        ? order.deliveryInstructions
-                                        : 'Инструкций нет..'}
-                                </p>
-                                <p className="text-gray-600 flex">
-                                    <RiSecurePaymentFill className="mr-2 mt-0.5" />
-                                    Метод оплаты:{' '}
-                                    {order.paymentMethod === 'cash'
-                                        ? 'Наличными курьеру'
-                                        : order.paymentMethod === 'card'
-                                        ? 'Картой'
-                                        : order.paymentMethod}
-                                </p>
-                                <p className="text-gray-600 flex">
-                                    <FaCalendarAlt className="mr-2 mt-0.5" />
-                                    Дата и время: {order.timestamp.toDate().toLocaleString()}
-                                </p>
-                                <h3 className="text-lg font-semibold mt-4">Позиции заказа:</h3>
-                                <ul className="list-disc pl-4">
-                                    {order.cartItems.map((item: IFoodItem) => (
-                                        <li key={item.id} className="text-gray-600">
-                                            <p>Название: {item.title}</p>
-                                            <p>Цена: {item.price} ₸</p>
-                                        </li>
-                                    ))}
-                                </ul>
+                                    <p className="text-gray-600 flex">
+                                        <FaSackDollar className="mr-2 mt-0.5" /> Итоговая цена: {order.totalPrice} ₸
+                                    </p>
+                                    <p className="text-gray-600 flex">
+                                        <FaBowlFood className="mr-2 mt-0.5" /> Статус заказа:{' '}
+                                        {order.orderStatus === 'pending' ? 'В обработке' : order.orderStatus}
+                                    </p>
+                                    <p className="text-gray-600 flex">
+                                        <TbTruckDelivery className="mr-2 mt-0.5" />
+                                        Статус доставки:{' '}
+                                        {order.deliveryStatus === 'pending' ? 'В обработке' : order.orderStatus}
+                                    </p>
+                                    <p className="text-gray-600 flex">
+                                        <FaClipboardList className="mr-2 mt-0.5" />
+                                        Инструкции курьеру:{' '}
+                                        {order.deliveryInstructions !== ''
+                                            ? order.deliveryInstructions
+                                            : 'Инструкций нет..'}
+                                    </p>
+                                    <p className="text-gray-600 flex">
+                                        <RiSecurePaymentFill className="mr-2 mt-0.5" />
+                                        Метод оплаты:{' '}
+                                        {order.paymentMethod === 'cash'
+                                            ? 'Наличными курьеру'
+                                            : order.paymentMethod === 'card'
+                                            ? 'Картой'
+                                            : order.paymentMethod}
+                                    </p>
+                                    <p className="text-gray-600 flex">
+                                        <FaCalendarAlt className="mr-2 mt-0.5" />
+                                        Дата и время: {formatDateTime(order.timestamp.toDate())}
+                                    </p>
+                                    <h3 className="text-lg font-semibold mt-4">Позиции заказа:</h3>
+                                    <div className="overflow-y-auto max-h-48">
+                                        <ul className="list-disc pl-4">
+                                            {order.cartItems.map((item: IFoodItem) => (
+                                                <li key={item.id} className="text-gray-600">
+                                                    <p>Название: {item.title}</p>
+                                                    <p>Цена: {item.price} ₸</p>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="mt-4 flex items-center justify-between">
+                                    <p className="text-gray-600 flex">
+                                        <FaMoneyCheckAlt className="mr-2 mt-0.5" />
+                                        Оплачено
+                                    </p>
+                                    <p className="text-green-500 flex">
+                                        <FaCheckCircle className="mr-2 mt-0.5" />
+                                        Успешно
+                                    </p>
+                                </div>
                             </div>
-                            <div className="mt-4 flex items-center justify-between">
-                                <p className="text-gray-600 flex">
-                                    <FaMoneyCheckAlt className="mr-2 mt-0.5" />
-                                    Оплачено
-                                </p>
-                                <p className="text-green-500 flex">
-                                    <FaCheckCircle className="mr-2 mt-0.5" />
-                                    Успешно
-                                </p>
-                            </div>
-                        </div>
-                    ))}
+                        ))
+                ) : (
+                    <p className="text-lg text-center text-gray-600">Нет доступных заказов.</p>
+                )}
             </div>
         </div>
     );
